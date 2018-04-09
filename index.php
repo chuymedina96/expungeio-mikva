c46521e70787d410dd330038203df9c8953e8876
Chuy Medina <chuymedina96@gmail.com>
2018-04-09 11:12:00 -0500

    adding everything

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This is the default-template when no default.html is added during the conversion.
+ * To create your own default-template during the converison consult our documentation.
+ *
+ * Your front-page can be found from front-page.php
+ */
+?>
